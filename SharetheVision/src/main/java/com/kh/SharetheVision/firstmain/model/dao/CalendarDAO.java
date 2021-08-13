2e5e81598112d0c184dfd22758c71569cc27c4ee
eunbinoh <nohonbi01@gmail.com>
2021-08-13 18:19:01 +0900

    calendar 일정추가 기능 업데이트

--- SharetheVision/src/main/java/com/kh/SharetheVision/firstmain/model/dao/CalendarDAO.java
+++ SharetheVision/src/main/java/com/kh/SharetheVision/firstmain/model/dao/CalendarDAO.java
@@ -2,12 +2,22 @@ package com.kh.SharetheVision.firstmain.model.dao;
 
 import java.util.ArrayList;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.kh.SharetheVision.firstmain.model.vo.Calendar;
 
-@Repository
-public interface CalendarDAO {
-	public ArrayList<Calendar> cList();
+@Repository("cdao")
+public class CalendarDAO {
+
+	public int addCal(SqlSessionTemplate sqlSession, Calendar c) {
+		System.out.println("dao IN !");
+		return sqlSession.insert("calendarMapper.addCal",c);
+	}
+
+//	public ArrayList<Calendar> addCal(SqlSessionTemplate sqlSession){
+//		
+//		return 
+//	}
 	
 }
