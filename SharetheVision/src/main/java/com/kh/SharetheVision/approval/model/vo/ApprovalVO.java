87551eec0e67890be7fc9f258fd0de92bf0c6a69
jieun <j1eune@naver.com>
2021-09-13 20:17:36 +0900

    결재 리스트 보기 (로그인 유저가 합의자, 참조자, 결재자, 기안자 일 때)

--- SharetheVision/src/main/java/com/kh/SharetheVision/approval/model/vo/ApprovalVO.java
+++ SharetheVision/src/main/java/com/kh/SharetheVision/approval/model/vo/ApprovalVO.java
@@ -6,142 +6,161 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class ApprovalVO {
 
-	private int apvNo;
-	private String apvType;
-	private String mCode;
-	private String apvAgr;
-	private String apvRef;
-	private String apvApp;
-	private String apt;
-	private String comment;
-	private Date arrive;
-	private Date depart;
-	private Date apvEdate;
-	private String apvStatus;
-	private int apvRefNo;
-	private MultipartFile fileObj;
+   private int apvNo;
+   private String apvType;
+   private String mCode;
+   private String apvAgr;
+   private String apvRef;
+   private String apvApp;
+   private String apt;
+   private String comment;
+   private Date arrive;
+   private Date depart;
+   private Date apvEdate;
+   private String apvStatus;
+   private int apvRefNo;
+   private MultipartFile fileObj;
+   private String atChange;
+   private String mName;
 
-	public ApprovalVO() {
-	}
+   public ApprovalVO() {
+   }
+   
+   public String getmName() {
+      return mName;
+   }
 
-	public int getApvNo() {
-		return apvNo;
-	}
+   public void setmName(String mName) {
+      this.mName = mName;
+   }
 
-	public void setApvNo(int apNo) {
-		this.apvNo = apNo;
-	}
+   public int getApvNo() {
+      return apvNo;
+   }
 
-	public String getApvType() {
-		return apvType;
-	}
+   public void setApvNo(int apvNo) {
+      this.apvNo = apvNo;
+   }
 
-	public void setApvType(String apvType) {
-		this.apvType = apvType;
-	}
+   public String getApvType() {
+      return apvType;
+   }
 
-	public String getmCode() {
-		return mCode;
-	}
+   public void setApvType(String apvType) {
+      this.apvType = apvType;
+   }
 
-	public void setmCode(String mCode) {
-		this.mCode = mCode;
-	}
+   public String getmCode() {
+      return mCode;
+   }
 
-	public String getApvAgr() {
-		return apvAgr;
-	}
+   public void setmCode(String mCode) {
+      this.mCode = mCode;
+   }
 
-	public void setApvAgr(String apvAgr) {
-		this.apvAgr = apvAgr;
-	}
+   public String getApvAgr() {
+      return apvAgr;
+   }
 
-	public String getApvRef() {
-		return apvRef;
-	}
+   public void setApvAgr(String apvAgr) {
+      this.apvAgr = apvAgr;
+   }
 
-	public void setApvRef(String apvRef) {
-		this.apvRef = apvRef;
-	}
+   public String getApvRef() {
+      return apvRef;
+   }
 
-	public String getApvApp() {
-		return apvApp;
-	}
+   public void setApvRef(String apvRef) {
+      this.apvRef = apvRef;
+   }
 
-	public void setApvApp(String apvApp) {
-		this.apvApp = apvApp;
-	}
+   public String getApvApp() {
+      return apvApp;
+   }
 
-	public String getApt() {
-		return apt;
-	}
+   public void setApvApp(String apvApp) {
+      this.apvApp = apvApp;
+   }
 
-	public void setApt(String apt) {
-		this.apt = apt;
-	}
+   public String getApt() {
+      return apt;
+   }
 
-	public String getComment() {
-		return comment;
-	}
+   public void setApt(String apt) {
+      this.apt = apt;
+   }
 
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
+   public String getComment() {
+      return comment;
+   }
 
-	public Date getArrive() {
-		return arrive;
-	}
+   public void setComment(String comment) {
+      this.comment = comment;
+   }
 
-	public void setArrive(Date arrive) {
-		this.arrive = arrive;
-	}
+   public Date getArrive() {
+      return arrive;
+   }
 
-	public Date getDepart() {
-		return depart;
-	}
+   public void setArrive(Date arrive) {
+      this.arrive = arrive;
+   }
 
-	public void setDepart(Date depart) {
-		this.depart = depart;
-	}
+   public Date getDepart() {
+      return depart;
+   }
 
-	public Date getApvEdate() {
-		return apvEdate;
-	}
+   public void setDepart(Date depart) {
+      this.depart = depart;
+   }
 
-	public void setApvEdate(Date apvEdate) {
-		this.apvEdate = apvEdate;
-	}
+   public Date getApvEdate() {
+      return apvEdate;
+   }
 
-	public String getApvStatus() {
-		return apvStatus;
-	}
+   public void setApvEdate(Date apvEdate) {
+      this.apvEdate = apvEdate;
+   }
 
-	public void setApvStatus(String apvStatus) {
-		this.apvStatus = apvStatus;
-	}
+   public String getApvStatus() {
+      return apvStatus;
+   }
 
-	public int getApvRefNo() {
-		return apvRefNo;
-	}
+   public void setApvStatus(String apvStatus) {
+      this.apvStatus = apvStatus;
+   }
 
-	public void setApvRefNo(int apvRefNo) {
-		this.apvRefNo = apvRefNo;
-	}
+   public int getApvRefNo() {
+      return apvRefNo;
+   }
 
-	public MultipartFile getFileObj() {
-		return fileObj;
-	}
+   public void setApvRefNo(int apvRefNo) {
+      this.apvRefNo = apvRefNo;
+   }
 
-	public void setFileObj(MultipartFile fileObj) {
-		this.fileObj = fileObj;
-	}
+   public MultipartFile getFileObj() {
+      return fileObj;
+   }
 
-	@Override
-	public String toString() {
-		return "Approval [apNo=" + apvNo + ", apvType=" + apvType + ", mCode=" + mCode + ", apvAgr=" + apvAgr
-				+ ", apvRef=" + apvRef + ", apvApp=" + apvApp + ", apt=" + apt + ", comment=" + comment + ", arrive="
-				+ arrive + ", depart=" + depart + ", apvEdate=" + apvEdate + ", apvStatus=" + apvStatus + ", apvRefNo="
-				+ apvRefNo + "]";
-	}
+   public void setFileObj(MultipartFile fileObj) {
+      this.fileObj = fileObj;
+   }
+   
+   public String getAtChange() {
+      return atChange;
+   }
+
+   public void setAtChange(String atChange) {
+      this.atChange = atChange;
+   }
+
+   @Override
+   public String toString() {
+      return "ApprovalVO [apvNo=" + apvNo + ", apvType=" + apvType + ", mCode=" + mCode + ", apvAgr=" + apvAgr
+            + ", apvRef=" + apvRef + ", apvApp=" + apvApp + ", apt=" + apt + ", comment=" + comment + ", arrive="
+            + arrive + ", depart=" + depart + ", apvEdate=" + apvEdate + ", apvStatus=" + apvStatus + ", apvRefNo="
+            + apvRefNo + ", fileObj=" + fileObj + ", atChange=" + atChange + ", mName=" + mName + "]";
+   }
+
 
 }
